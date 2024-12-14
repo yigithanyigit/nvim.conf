e4fead2b92c4f84cf45343673bc667db316fe6d5
Yigithan Yigit <yigithanyigitdevel@gmail.com>
2024-12-14 17:50:37 +0300

    update lspconfig for better LSP server handling

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -52,20 +52,21 @@ M.capabilities.textDocument.completion.completionItem = {
   },
 }
 
+local function setup_server(server, config)
+  config = config or {}
+  config.on_attach = M.on_attach
+  config.capabilities = M.capabilities
+  config.on_init = M.on_init
+  require("lspconfig")[server].setup(config)
+end
+
 M.defaults = function()
   dofile(vim.g.base46_cache .. "lsp")
   require("nvchad.lsp").diagnostic_config()
 
-  require("lspconfig").clangd.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-  }
-
-  require("lspconfig").lua_ls.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-
+  setup_server("pyright")
+  setup_server("clangd")
+  setup_server("lua_ls", {
     settings = {
       Lua = {
         diagnostics = {
@@ -84,32 +85,7 @@ M.defaults = function()
         },
       },
     },
-  }
-
-  require("lspconfig").lua_ls.setup {
-    on_attach = M.on_attach,
-    capabilities = M.capabilities,
-    on_init = M.on_init,
-
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim" },
-        },
-        workspace = {
-          library = {
-            vim.fn.expand "$VIMRUNTIME/lua",
-            vim.fn.expand "$VIMRUNTIME/lua/vim/lsp",
-            vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
-            vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
-            "${3rd}/luv/library",
-          },
-          maxPreload = 100000,
-          preloadFileSize = 10000,
-        },
-      },
-    },
-  }
+  })
 end
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -163,7 +163,7 @@ return {
     cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
     build = ":TSUpdate",
     opts = function()
-      return require "nvchad.configs.treesitter"
+      return require "configs.treesitter"
     end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
